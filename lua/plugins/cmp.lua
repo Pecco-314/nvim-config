d98c2ed4beef2fb37c117d6ecc2e240fdd27d14c
Pecco <pecco@qq.com>
2023-12-04 23:43:43 +0800

    微调：移动代码位置

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,9 +1,3 @@
-local has_words_before = function()
-    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-end
-
 return {
     {
         "hrsh7th/nvim-cmp",
@@ -21,6 +15,11 @@ return {
         config = function()
             local lspkind = require('lspkind')
             local cmp = require('cmp')
+            local has_words_before = function()
+                if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+            end
             cmp.setup({
                 snippet = {
                     expand = function(args)
