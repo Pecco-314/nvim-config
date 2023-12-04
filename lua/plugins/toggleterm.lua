38b8c1738785b40f5517752ccf08319ed6e13ddf
Pecco <pecco@qq.com>
2023-12-05 01:46:17 +0800

    修改终端配置

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -8,19 +8,35 @@ return {
             }
             local term = require("toggleterm.terminal")
 
-            local main = term.Terminal:new({
-                hidden = true,
-                direction = "horizontal",
-                on_open = function()
-                    vim.cmd("resize 16")
-                    vim.cmd("startinsert!")
-                end,
-            })
+            local function terminal(id)
+                id = id or 1
+                return term.Terminal:new({
+                    id = id,
+                    hidden = true,
+                    direction = "float",
+                    on_create = function(t)
+                        t:send("clear")
+                    end,
+                    on_open = function()
+                        vim.cmd("startinsert!")
+                    end,
+                    float_opts = {
+                        border = "curved",
+                        width = 120,
+                        height = 20,
+                        winblend = 3,
+                    },
+                })
+            end
 
             local lazygit = term.Terminal:new({
                 cmd = "lazygit",
+                id = 101,
                 hidden = true,
                 direction = "float",
+                on_open = function()
+                    vim.cmd("startinsert!")
+                end,
                 float_opts = {
                     border = "curved",
                     width = 120,
@@ -33,12 +49,19 @@ return {
                 lazygit:toggle()
             end
 
-            local function toggleMain()
-                main:toggle()
+            local function toggleTerminal(id)
+                id = id or 1
+                terminal(id):toggle()
             end
 
-            vim.keymap.set("n", "<M-t>", toggleMain, OPTS)
-            vim.keymap.set("t", "<M-t>", toggleMain, OPTS)
+            local function toggleTerminalWithInputId()
+                local id = vim.fn.nr2char(vim.fn.getchar())
+                toggleTerminal(id)
+            end
+
+            vim.keymap.set("n", "<M-t>", toggleTerminal, OPTS)
+            vim.keymap.set("t", "<M-t>", toggleTerminal, OPTS)
+            vim.keymap.set("n", "<leader>t", toggleTerminalWithInputId, OPTS)
             vim.keymap.set("n", "<M-g>", toggleLazygit, OPTS)
             vim.keymap.set("t", "<M-g>", toggleLazygit, OPTS)
         end
