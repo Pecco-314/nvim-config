3ee8362118044eeaa74ab318c1661a137315df8d
Pecco <pecco@qq.com>
2023-12-04 23:37:34 +0800

    增加notify和noice插件

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "rcarriga/nvim-notify",
+        event = "VeryLazy",
+        config = function()
+            local notify = require("notify")
+            notify.setup()
+            vim.notify = notify
+        end
+    }
+}
