cc2e5e60429dfe4c7507b46ccf7ea949fe504135
Pecco <pecco@qq.com>
2023-12-06 17:50:05 +0800

    修改telescope配置

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,27 +8,31 @@ return {
             "folke/todo-comments.nvim"
         },
         config = function()
+            local telescope = require("telescope")
             local bt = require("telescope.builtin")
+            local ext = telescope.extensions
+
             local function find_another_file()
                 local path = vim.fn.input('Enter a path: ', vim.fn.getcwd(), 'dir')
                 bt.find_files({ cwd = path })
             end
-            vim.keymap.set("n", "<M-p>", [[<Cmd>Telescope find_files<CR>]], OPTS)
-            vim.keymap.set("n", "<C-M-p>", [[<Cmd>Telescope live_grep<CR>]], OPTS)
-            vim.keymap.set("n", "<F1>", [[<Cmd>Telescope help_tags<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gk", [[<Cmd>Telescope keymaps<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gh", [[<Cmd>Telescope highlights<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gR", [[<Cmd>Telescope registers<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gb", [[<Cmd>Telescope buffers<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gg", [[<Cmd>Telescope git_status<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gc", [[<Cmd>Telescope git_commits<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gr", [[<Cmd>Telescope lsp_references<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gs", [[<Cmd>Telescope lsp_document_symbols<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gS", [[<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gd", [[<Cmd>Telescope lsp_diagostics<CR>]], OPTS)
-            vim.keymap.set("n", "<F12>", [[<Cmd>Telescope noice<CR>]], OPTS)
-            vim.keymap.set("n", "<leader>gt", [[<Cmd>telescope todo-comments<CR>]], OPTS)
+
+            vim.keymap.set("n", "<M-p>", function() bt.find_files({ cwd = vim.fn.getcwd() }) end, OPTS)
+            vim.keymap.set("n", "<C-M-p>", bt.live_grep, OPTS)
+            vim.keymap.set("n", "<F1>", bt.help_tags, OPTS)
             vim.keymap.set("n", "<leader>gf", find_another_file, OPTS)
+            vim.keymap.set("n", "<leader>gk", bt.keymaps, OPTS)
+            vim.keymap.set("n", "<leader>gh", bt.highlights, OPTS)
+            vim.keymap.set("n", "<leader>g'", bt.registers, OPTS)
+            vim.keymap.set("n", "<leader>gb", bt.buffers, OPTS)
+            vim.keymap.set("n", "<leader>gd", bt.diagnostics, OPTS)
+            vim.keymap.set("n", "<leader>gg", bt.git_status, OPTS)
+            vim.keymap.set("n", "<leader>gc", bt.git_commits, OPTS)
+            vim.keymap.set("n", "<leader>gr", bt.lsp_references, OPTS)
+            vim.keymap.set("n", "<leader>gs", bt.lsp_document_symbols, OPTS)
+            vim.keymap.set("n", "<leader>gw", bt.lsp_dynamic_workspace_symbols, OPTS)
+            vim.keymap.set("n", "<leader>gt", ext["todo-comments"].todo, OPTS)
+            vim.keymap.set("n", "<F12>", ext["noice"].noice, OPTS)
         end,
     }
 }
