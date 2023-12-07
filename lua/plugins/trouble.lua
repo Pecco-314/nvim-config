cb9c80d35e5eca9afe65d5ffd44b11f47b345c75
Pecco <pecco@qq.com>
2023-12-07 17:57:19 +0800

    删除trouble插件（telescope基本可以覆盖其功能）

--- lua/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function ()
-        require("trouble").setup()
-        vim.keymap.set("n", "<M-d>", "<Cmd>TroubleToggle<CR>")
-    end
-}
