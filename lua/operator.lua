798fa14de55b45cb3d3a9de4eb63099967557089
Pecco <pecco@qq.com>
2023-12-08 22:13:23 +0800

    拓展自定义的s操作符的功能

--- lua/operator.lua
+++ lua/operator.lua
@@ -1,12 +1,42 @@
 local util = require("util")
 
-function _G.substitude()
-    local start = vim.api.nvim_buf_get_mark(0, '[')
-    local finish = vim.api.nvim_buf_get_mark(0, ']')
+local function substitude(opts)
+    opts = opts or {}
+    local reg = opts.reg or '"'
+    local mode = opts.mode or 'n'
+    local start, finish
+    if mode == 'n' then
+        start = vim.api.nvim_buf_get_mark(0, '[')
+        finish = vim.api.nvim_buf_get_mark(0, ']')
+    elseif mode == 'v' or mode == 'V' then
+        start = vim.api.nvim_buf_get_mark(0, '<')
+        finish = vim.api.nvim_buf_get_mark(0, '>')
+        if mode == 'V' then
+            start[2] = 0
+            finish[2] = vim.fn.col('$') - 1
+        end
+    else
+        vim.notify('cannot substitude in mode ' .. mode, vim.log.levels.WARN)
+        return
+    end
+    if finish[2] == vim.fn.col('$') - 1 then
+        finish[2] = finish[2] - 1
+    end
+    if start[1] > finish[1] then -- textobj-line选中空行时会出现这种情况
+        finish[1] = start[1]
+        finish[2] = start[2] - 1
+    end
     local bufid = vim.fn.bufnr()
-    local newtext = vim.fn.getreg('"')
-    local newtexttable = util.split(newtext)
+    local newtext = vim.fn.getreg(reg)
+    local newtexttable = util.split(newtext, '\n')
     vim.api.nvim_buf_set_text(bufid, start[1] - 1, start[2], finish[1] - 1, finish[2] + 1, newtexttable)
 end
 
-vim.keymap.set('n', 's', util.operator("substitude"), OPTS)
+vim.keymap.set('n', 's', function() return util.operator(substitude) end, OPTS)
+vim.keymap.set('v', 's', function() return util.operator(substitude, { mode = vim.fn.mode() }) end, OPTS)
+
+for _, reg in ipairs(util.tolist(util.REGS)) do
+    vim.keymap.set('n', '"' .. reg .. 's', function() return util.operator(substitude, { reg = reg }) end, OPTS)
+    vim.keymap.set('v', '"' .. reg .. 's',
+        function() return util.operator(substitude, { reg = reg, mode = vim.fn.mode() }) end, OPTS)
+end
--- lua/util.lua
+++ lua/util.lua
@@ -1,16 +1,30 @@
 local M = {}
 
-function M.operator(funcname)
-    vim.go.operatorfunc = "v:lua." .. funcname
-    return 'g@'
+function M.operator(func, opts)
+    local function adapter()
+        return func(opts)
+    end
+    _G.operatorfunc_adapter = adapter
+    vim.go.operatorfunc = "v:lua.operatorfunc_adapter"
+    vim.api.nvim_feedkeys("g@", "n", false)
 end
 
-function M.split(text)
-    local lines = {}
-    for line in text:gmatch("[^\r\n]+") do
-        table.insert(lines, line)
+function M.split(text, sep)
+    local list = {}
+    for str in text:gmatch("[^" .. sep .. "]+") do
+        table.insert(list, str)
     end
-    return lines
+    return list
 end
+
+function M.tolist(text)
+    local list = {}
+    for char in text:gmatch(".") do
+        table.insert(list, char)
+    end
+    return list
+end
+
+M.REGS = [[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.:+-*/#="]]
 
 return M
