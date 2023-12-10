2c20d72e59daa29a8ccf370359f1c28c5b708efc
Pecco <pecco@qq.com>
2023-12-10 17:00:16 +0800

    拦截帮助文档改为使用BufEnter事件而不是FileType

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,6 +1,8 @@
 local util = require('util')
 
 function _G.openPopupHelp()
+    -- 判断是否是帮助文档
+    if vim.bo.filetype ~= 'help' then return end
     local bufnr = vim.api.nvim_get_current_buf()
     local win = vim.api.nvim_get_current_win()
     vim.api.nvim_win_close(win, true)
@@ -8,4 +10,4 @@ function _G.openPopupHelp()
 end
 
 -- 拦截帮助文档
-vim.cmd([[autocmd FileType help lua openPopupHelp()]])
+vim.cmd([[autocmd BufEnter * lua openPopupHelp()]])
