6f7e923bb2fd0ac6d275b5f389ac21b841f64fc3
Pecco <pecco@qq.com>
2023-12-12 22:02:25 +0800

    补充各个插件的Event

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,6 +1,7 @@
 return {
     {
         "phaazon/hop.nvim",
+        event = "VeryLazy",
         config = function()
             require("hop").setup()
             vim.keymap.set("n", "<F3>", [[<Cmd>HopChar1<CR>]], OPTS)
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,7 @@
 return {
     {
         "akinsho/toggleterm.nvim",
+        event = "VeryLazy",
         config = function()
             require("toggleterm").setup {
                 direction = 'horizontal',
