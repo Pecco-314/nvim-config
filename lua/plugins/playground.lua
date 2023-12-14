0e1a774afc9e2d73d352761d79fbf55b72ef61f8
Pecco <pecco@qq.com>
2023-12-14 23:05:07 +0800

    添加playground插件

---
+++ lua/plugins/playground.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "nvim-treesitter/playground",
+        event = "VeryLazy",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                playground = {
+                    enable = true,
+                    disable = {},
+                    updatetime = 25,
+                    persist_queries = false,
+                    keybindings = {
+                        toggle_query_editor = "o",
+                        toggle_hl_groups = "i",
+                        toggle_injected_languages = "t",
+                        toggle_anonymous_nodes = "a",
+                        toggle_language_display = "I",
+                        focus_language = "f",
+                        unfocus_language = "F",
+                        update = "R",
+                        goto_node = "<cr>",
+                        show_help = "?",
+                    },
+                },
+            })
+            vim.keymap.set("n", "<M-r>", ":TSPlaygroundToggle<CR>", OPTS)
+            vim.keymap.set("n", "J", ":TSHighlightCapturesUnderCursor<CR>", OPTS)
+            vim.keymap.set("n", ";", ":TSNodeUnderCursor<CR>", OPTS)
+        end
+    }
+}
