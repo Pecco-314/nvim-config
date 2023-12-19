b7bdc7958ae9a91ad90533321604655ab3f39b35
Pecco <pecco@qq.com>
2023-12-19 22:13:34 +0800

    不再于nvim-tree中显示.git文件夹

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,10 @@ return {
             require("nvim-tree").setup({
                 respect_buf_cwd = true,
                 sync_root_with_cwd = true,
+                filters = {
+                    dotfiles = true,
+                    custom = { ".git" }
+                },
                 actions = {
                     open_file = {
                         quit_on_open = true,
